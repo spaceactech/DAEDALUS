59fa6c5b702de8fb44a374752582d3416b0499ba
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2026-03-18 13:46:26 +0700

    gfyukyugj

--- src/test_half/main.cpp
+++ src/test_half/main.cpp
@@ -1,49 +1,45 @@
 #include <SCServo.h>
 #include "UserPins.h"
-#include <Arduino.h>
+#include <Arduino_Extended.h>
 
 SMS_STS        sms_sts;
-HardwareSerial HalfSerial(USER_GPIO_Half, USER_GPIO_Half);
-uint8_t ID[] = {1, 2};
-byte    ACC[2];
-uint8_t rxPacket[4];
+HardwareSerial HalfSerial(USER_GPIO_Half);
+uint8_t        servo_ids[]     = {1, 2};
+byte           servo_accels[2] = {50, 50};
+uint8_t        rxPacket[4];
 
-int16_t Position;
-int16_t Speed;
+int Position;
+int Speed;
 
 void setup() {
-  Serial.begin(115200);       //sms舵机波特率115200
-  HalfSerial.begin(1000000);  //sts舵机波特率1000000
-  HalfSerial.setHalfDuplex();
+  Serial.begin(115200);
+  HalfSerial.begin(115200);
   sms_sts.pSerial = &HalfSerial;
   delay(1000);
-  sms_sts.WheelMode(1);
-  sms_sts.WheelMode(2);                                    //舵机ID1切换至电机恒速模式
-  sms_sts.syncReadBegin(sizeof(ID), sizeof(rxPacket), 5);  //10*10*2=200us<5ms
-  HalfSerial.enableHalfDuplexRx();
-  delay(150);
+
+  sms_sts.syncReadBegin(sizeof(servo_ids), sizeof(rxPacket), 5);
+  for (size_t i = 0; i < sizeof(servo_ids); ++i) {
+    sms_sts.WheelMode(i);
+    sms_sts.WriteSpe(i, 1000, servo_accels[i]);
+  }
 }
 
 void loop() {
-  sms_sts.WriteSpe(1, 1000, 50);
-  sms_sts.WriteSpe(2, 1000, 50);
+  sms_sts.syncReadPacketTx(servo_ids, sizeof(servo_ids), SMS_STS_PRESENT_POSITION_L, sizeof(rxPacket));
 
-  HalfSerial.flush();
-  delay(150);
-  HalfSerial.enableHalfDuplexRx();
-  sms_sts.syncReadPacketTx(ID, sizeof(ID), SMS_STS_PRESENT_POSITION_L, sizeof(rxPacket));  //同步读指令包发送
-  for (uint8_t i = 0; i < sizeof(ID); i++) {
-    //接收ID[i]同步读返回包
-    if (!sms_sts.syncReadPacketRx(ID[i], rxPacket)) {
+  for (size_t i = 0; i < sizeof(servo_ids); ++i) {
+    if (!sms_sts.syncReadPacketRx(servo_ids[i], rxPacket)) {
       Serial.print("ID:");
-      Serial.println(ID[i]);
+      Serial.println(servo_ids[i]);
       Serial.println("sync read error!");
-      continue;  //接收解码失败
+      continue;
     }
-    Position = sms_sts.syncReadRxPacketToWrod(15);  //解码两个字节 bit15为方向位,参数=0表示无方向位
-    Speed    = sms_sts.syncReadRxPacketToWrod(15);  //解码两个字节 bit15为方向位,参数=0表示无方向位
+
+    Position = sms_sts.syncReadRxPacketToWrod(15);
+    Speed    = sms_sts.syncReadRxPacketToWrod(15);
+
     Serial.print("ID:");
-    Serial.println(ID[i]);
+    Serial.println(servo_ids[i]);
     Serial.print("Position:");
     Serial.println(Position);
     Serial.print("Speed:");
